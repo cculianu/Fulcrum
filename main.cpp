9eec882fbdc1a250c75c5d51d2fd81371784abfc
Calin Culianu <calin.culianu@gmail.com>
2019-04-25 21:20:45 +0300

    removed testign

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <QCoreApplication>
 #include "App.h"
-#include "BTC.h"
 
 int main(int argc, char *argv[])
 {
-    return BTC::Address::test();
     App app(argc, argv);
 
     return app.exec();
