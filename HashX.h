f97a00088366417e0cc246caf9935fad8caf74ca
Calin Culianu <calin.culianu@gmail.com>
2019-12-02 14:32:26 +0200

    work around compile bugs on windows mingw g++

--- HashX.h
+++ HashX.h
@@ -5,6 +5,8 @@
 
 #include <QByteArray>
 
+#include <utility>
+
 /// "HashX" (ElectrumX/ElectronX style hash) which is a sha256 hash, done once, and "pre-reversed"
 /// (that is, ready to be converted to hex for transmission directly as-is).
 /// We inherit from QByteArray so as to benefit from its implicit sharing and other untility functions.
@@ -13,6 +15,11 @@ struct HashX : public QByteArray {
     using QByteArray::QByteArray;
     /// construct from a CScript by taking the 32-byte *reversed* sha256 once of the CScript data.
     HashX(const bitcoin::CScript &);
+
+    /// This c'tor is needed to work around bugs on MinGW G++
+    HashX(const HashX & other) : QByteArray(other) {}
+    /// Workaround for bugs in MinGW G++
+    HashX(HashX && other) : QByteArray(std::move(other)) {}
 
     static HashX fromCScript(const bitcoin::CScript &);
     /// Faster fromHex() which should be used only when you are sure the incoming data is definitely hex.
@@ -23,6 +30,10 @@ struct HashX : public QByteArray {
     QByteArray toHex() const;
 
     HashX & operator=(const bitcoin::CScript &);
+
+    /// Work around MinGW G++ bugs
+    HashX & operator=(const HashX &other) { QByteArray::operator=(other); return *this; }
+    HashX & operator=(HashX && other) { QByteArray::operator=(std::move(other)); return *this; }
 
     /// we had to explicitly redefine this because 'using' it created some compile errors about ambiguous
     /// overloads (likely there are some private methods we inadvertently brought in?)
