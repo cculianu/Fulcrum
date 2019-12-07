0402e9de96834ed0008a7615fb7d596232186b6f
Calin Culianu <calin.culianu@gmail.com>
2019-12-07 15:08:52 +0200

    nits

--- BlockProcTypes.h
+++ BlockProcTypes.h
@@ -7,16 +7,11 @@
 
 #include <QByteArray>
 
-#include <functional>
-
 using HashHasher = BTC::QByteArrayHashHasher;
 
-using TxNum = std::uint64_t;
 using BlockHeight = std::uint32_t;
 using IONum = std::uint16_t;
 using TxHash = QByteArray;
 using HashX = QByteArray;
 static constexpr int HashLen = bitcoin::uint256::width();
 
-using TxHash2NumResolver = std::function< std::optional<TxNum>(const TxHash &) >;
-using Num2TxHashResolver = std::function< std::optional<TxHash>(TxNum) >;
