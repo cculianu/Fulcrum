674008dea3e544f8e8a8853c3b1d873ab38a6e35
Calin Culianu <calin.culianu@gmail.com>
2020-01-09 03:33:54 +0200

    Forgot the copyright message at the top of PeerMgr.cpp

--- PeerMgr.cpp
+++ PeerMgr.cpp
@@ -1,3 +1,21 @@
+//
+// Fulcrum - A fast & nimble SPV Server for Bitcoin Cash
+// Copyright (C) 2019-2020  Calin A. Culianu <calin.culianu@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program (see LICENSE.txt).  If not, see
+// <https://www.gnu.org/licenses/>.
+//
 #include "PeerMgr.h"
 
 #include "Options.h"
