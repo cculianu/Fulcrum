c4544c8bb656a2ba04c798c57eae54106362d1d6
Axel Gembe <derago@gmail.com>
2020-01-11 18:42:41 +0700

    Build: Add script to start the docker build

---
+++ contrib/base.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$CPU_COUNT" ] ; then
+    # CPU_COUNT is not set, try to detect the core count
+    case $(uname) in
+        Linux)
+            export CPU_COUNT=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+            ;;
+        Darwin)
+            export CPU_COUNT=$(sysctl -n hw.ncpu)
+            ;;
+    esac
+fi
+# If CPU_COUNT is still unset, default to 4
+export CPU_COUNT="${CPU_COUNT:-4}"
+# Use one more worker than core count
+export WORKER_COUNT=$[$CPU_COUNT+1]
