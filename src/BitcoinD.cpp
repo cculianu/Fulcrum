b1d7252a553e33bb49fab62cf314b82489940555
Calin Culianu <calin.culianu@gmail.com>
2020-03-17 03:03:50 +0200

    Removed an unused lambda capture

--- src/BitcoinD.cpp
+++ src/BitcoinD.cpp
@@ -295,7 +295,7 @@ void BitcoinD::on_started()
                 return false; // kill timer
             });
         };
-        conns += connect(this, &BitcoinD::lostConnection, this, [SetTimer, this]{
+        conns += connect(this, &BitcoinD::lostConnection, this, [SetTimer]{
             Log() << "Lost connection to bitcoind, will retry every 5 seconds ...";
             SetTimer();
         });
