f95c227067a1951834447cd3c2a6d9190321e024
Calin Culianu <calin.culianu@gmail.com>
2020-07-18 03:42:33 +0300

    ByteView template class fixups
    - Got rid of all constexpr methods -- they relied on casting through
    void * which is not allowed in constexpr. Even though the compiler
    didn't complain, casting through void * in a constexpr is not portable
    across all compilers, since the standard says it's forbidden. In practice
    the lack of constexpr doesn't cost us much -- the optimizer will always
    optimize ByteView away to being a zero-cost abstraction if using -O2 or
    above.
    
    - Made ByteView work implicitly with std::array of POD data.
    
    - Made ByteView template c'tors reject C arrays that are arrays of pointers.

--- src/ByteView.h
+++ src/ByteView.h
@@ -43,40 +43,44 @@
 class ByteView: public std::basic_string_view<std::byte>
 {
     template <typename CharT, typename T, typename = std::enable_if_t<sizeof(CharT) == 1>>
-    static constexpr const CharT *ptr_cast(const T *t) {
-        // This trick gets past the "no reinterpret_cast in constexpr" rule.
-        // I wasn't sure if the below was an aliasing rule violation, but apparently it's not:
-        //   https://old.reddit.com/r/cpp/comments/hs6ut6/is_this_a_legal_use_of_pointer_casting_strict/
-        return static_cast<const CharT *>(static_cast<const void *>(t));
+    static const CharT *ptr_cast(const T *t) noexcept {
+        // This single line of code here prevents all the methods of this class from being constexpr :/
+        return reinterpret_cast<const CharT *>(t);
     }
+    // type trait to exclude C arrays but accept std::array
+    template<typename T> struct is_std_array : std::false_type {};
+    template<typename T, std::size_t N> struct is_std_array<std::array<T,N>> : std::true_type{};
+    template<typename T> static constexpr bool is_std_array_v = is_std_array<T>::value;
+
 public:
     using Base = std::basic_string_view<std::byte>;
     using Base::basic_string_view; // inherit constructors
 
-    /// Construct a ByteView from any POD data item.
-    template<typename T, std::enable_if_t<std::is_pod_v<T> && !std::is_pointer_v<T>, int> = 0>
-    constexpr ByteView(const T &t) noexcept
+    /// Construct a ByteView from any POD data item or C array (but not a std::array)
+    template<typename T, std::enable_if_t<std::is_pod_v<T> && !std::is_pointer_v<T> && !is_std_array_v<T>
+                                          && (!std::is_array_v<T> || !std::is_pointer_v<std::remove_all_extents_t<T>>), int> = 0>
+    ByteView(const T &t) noexcept
         : ByteView(ptr_cast<std::byte>(&t), sizeof(t)) {}
 
     /// Construct a ByteView from any container that offers a .data() method that is a pointer to POD data,
-    /// and a .size() method -- such as QByteArray, std::vector, QString, etc.
+    /// and a .size() method -- such as QByteArray, std::vector, QString, std::array, etc.
     template<typename T,
              std::enable_if_t<std::is_same_v<QString, T> /* special case for QString */ ||
                               (std::is_pointer_v<decltype(std::declval<const T>().data())>
                                && std::is_integral_v<decltype(std::declval<const T>().size())>
                                && std::is_pod_v<std::remove_pointer_t<decltype(std::declval<const T>().data())>>
                                && !std::is_pointer_v<std::remove_pointer_t<decltype(std::declval<const T>().data())>>), int> = 0>
-    constexpr ByteView(const T &t) noexcept
+    ByteView(const T &t) noexcept
         : ByteView(ptr_cast<std::byte>(t.data()), t.size() * sizeof(*t.data())) {
         static_assert (!std::is_same_v<T, QString>
                        || (std::is_same_v<const QChar *, decltype(t.data())> && sizeof(QChar) == sizeof(uint16_t)
                            && std::has_unique_object_representations_v<QChar>),
                        "Assumption for QString is that QChar is essentially a uint16_t");
     }
-    constexpr const char * charData() const noexcept { return ptr_cast<char>(data()); }
-    constexpr const uint8_t * ucharData() const noexcept { return ptr_cast<uint8_t>(data()); }
+    const char * charData() const noexcept { return ptr_cast<char>(data()); }
+    const uint8_t * ucharData() const noexcept { return ptr_cast<uint8_t>(data()); }
 
-    constexpr std::string_view toStringView() const noexcept { return std::string_view{charData(), size()}; }
+    std::string_view toStringView() const noexcept { return std::string_view{charData(), size()}; }
 
     QByteArray toByteArray(bool deepCopy = true) const {
         return deepCopy ? QByteArray{charData(), int(size())}
@@ -85,6 +89,6 @@ public:
 };
 
 /// String literal -> ByteView e.g.: "foo"_bv or "\x01\xff\x07\xab"_bv
-inline constexpr ByteView operator "" _bv(const char *str, std::size_t len) noexcept {
+inline ByteView operator "" _bv(const char *str, std::size_t len) noexcept {
     return std::string_view{str, len}; // goes through template c'tor above..
 }
