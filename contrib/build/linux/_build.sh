5f11bc6d293c12bbcf84147b988a7add3506c17d
Calin Culianu <calin.culianu@gmail.com>
2020-08-28 02:09:40 +0300

    added building of jemalloc to linux docker build

--- contrib/build/linux/_build.sh
+++ contrib/build/linux/_build.sh
@@ -17,6 +17,23 @@ top=/work
 cd "$top" || fail "Could not cd $top"
 . "$top/$PACKAGE/contrib/build/common/common.sh" || (echo "Cannot source common.h" && exit 1)
 
+info "Running configure for jemalloc ..."
+cd "$JEMALLOC_PACKAGE" || fail "Could not change dir to $JEMALLOC_PACKAGE"
+./autogen.sh -with-jemalloc-prefix= --disable-shared --enable-static \
+    || fail "Configure of jemalloc failed"
+
+info "Building jemalloc ..."
+make -j`nproc` || fail "Could not build jemalloc"
+make install || fail "Could not install jemalloc"
+JEMALLOC_LIBDIR=$(jemalloc-config --libdir)
+[ -n "$JEMALLOC_LIBDIR" ] || fail "Could not determine JEMALLOC_LIBDIR"
+for a in "$JEMALLOC_LIBDIR"/jemalloc*; do
+    bn=`basename $a`
+    info "Stripping $bn ..."
+    strip -g "$a" || fail "Failed to strip $a"
+done
+printok "jemalloc static library built and installed in $JEMALLOC_LIBDIR"
+
 info "Building RocksDB ..."
 cd "$top/$ROCKSDB_PACKAGE" || fail "Could not cd tp $ROCKSDB_PACKAGE"
 USE_RTTI=1 PORTABLE=1 make static_lib -j`nproc` V=1 || fail "Could not build RocksDB"
@@ -31,7 +48,9 @@ printok "RocksDB built and moved to Fulcrum staticlibs directory"
 cd "$top"/"$PACKAGE" || fail "Could not chdir to Fulcrum dir"
 
 info "Building Fulcrum ..."
-qmake || fail "Could not run qmake"
+mkdir build && cd build || fail "Could not create/change-to build/"
+qmake ../Fulcrum.pro "CONFIG-=debug" "CONFIG+=release" "LIBS+=-L${JEMALLOC_LIBDIR} -ljemalloc" \
+    || fail "Could not run qmake"
 make -j`nproc` || fail "Could not run make"
 
 ls -al "$TARGET_BINARY" || fail "$TARGET_BINARY not found"
