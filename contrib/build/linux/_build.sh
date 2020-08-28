c62d317996f8199d472966dbb07190df3d31050a
Calin Culianu <calin.culianu@gmail.com>
2020-08-28 12:22:16 +0300

    debug test

--- contrib/build/linux/_build.sh
+++ contrib/build/linux/_build.sh
@@ -28,20 +28,20 @@ make -j`nproc` || fail "Could not build jemalloc"
 make install || fail "Could not install jemalloc"
 JEMALLOC_LIBDIR=$(jemalloc-config --libdir)
 [ -n "$JEMALLOC_LIBDIR" ] || fail "Could not determine JEMALLOC_LIBDIR"
-for a in "$JEMALLOC_LIBDIR"/libjemalloc*; do
-    bn=`basename $a`
-    info "Stripping $bn ..."
-    strip -g "$a" || fail "Failed to strip $a"
-done
+#for a in "$JEMALLOC_LIBDIR"/libjemalloc*; do
+#    bn=`basename $a`
+#    info "Stripping $bn ..."
+#    strip -g "$a" || fail "Failed to strip $a"
+#done
 printok "jemalloc static library built and installed in $JEMALLOC_LIBDIR"
 
 info "Building RocksDB ..."
 cd "$top/$ROCKSDB_PACKAGE" || fail "Could not cd tp $ROCKSDB_PACKAGE"
-USE_RTTI=1 PORTABLE=1 DISABLE_JEMALLOC=1 make static_lib -j`nproc` V=1 \
+USE_RTTI=1 PORTABLE=1 make static_lib -j`nproc` V=1 \
     || fail "Could not build RocksDB"
 
 info "Stripping librocksdb.a ..."
-strip -g librocksdb.a || fail "Could not strip librocksdb.a"
+#strip -g librocksdb.a || fail "Could not strip librocksdb.a"
 
 info "Copying librocksdb.a to Fulcrum directory ..."
 cp -fpva librocksdb.a "$top"/"$PACKAGE"/staticlibs/rocksdb/bin/linux || fail "Could not copy librocksdb.a"
@@ -51,7 +51,7 @@ cd "$top"/"$PACKAGE" || fail "Could not chdir to Fulcrum dir"
 
 info "Building Fulcrum ..."
 mkdir build && cd build || fail "Could not create/change-to build/"
-qmake ../Fulcrum.pro "CONFIG-=debug" "CONFIG+=release" "LIBS+=-L${JEMALLOC_LIBDIR} -ljemalloc" \
+qmake ../Fulcrum.pro "CONFIG-=release" "CONFIG+=debug" "LIBS+=-L${JEMALLOC_LIBDIR} -ljemalloc" \
     || fail "Could not run qmake"
 make -j`nproc` || fail "Could not run make"
 
