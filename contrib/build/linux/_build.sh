5edeaa9b8b87ce6d32650fd276143b00e4f4bc2f
Calin Culianu <calin.culianu@gmail.com>
2020-08-28 15:19:08 +0300

    Add jemalloc static library to the static library builder for Windows & Linux (#56)
    This makes the docker-based static library builder (`contrib/build/build.sh`) 
    clone and compile jemalloc. It then links the jemalloc library statically the 
    generated static executable.
    
    Tested and works on both Linux and Windows.
    
    Also in this commit: Added a "Jemalloc" key to the stats outputted by the 
    "/stats" HTTP endpoint, which includes some of the output of `malloc_print_stats()` 
    from Jemalloc.  Note that for this to contain any data, the jemalloc library must be 
    linked at compile-time.

--- contrib/build/linux/_build.sh
+++ contrib/build/linux/_build.sh
@@ -4,22 +4,43 @@
 
 set -e  # Exit on error
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Please pass Fulcrum and rocksdb dirnames as the two args"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "Please pass Fulcrum, rocksdb, and jemalloc dirnames as the three args"
     exit 1
 fi
 
 PACKAGE="$1"
 ROCKSDB_PACKAGE="$2"
+JEMALLOC_PACKAGE="$3"
 TARGET_BINARY=Fulcrum
 
 top=/work
 cd "$top" || fail "Could not cd $top"
 . "$top/$PACKAGE/contrib/build/common/common.sh" || (echo "Cannot source common.h" && exit 1)
 
+info "Running configure for jemalloc ..."
+cd "$JEMALLOC_PACKAGE" || fail "Could not change dir to $JEMALLOC_PACKAGE"
+./autogen.sh --with-jemalloc-prefix= --disable-shared --enable-static \
+    || fail "Configure of jemalloc failed"
+
+info "Building jemalloc ..."
+make -j`nproc` || fail "Could not build jemalloc"
+make install || fail "Could not install jemalloc"
+JEMALLOC_LIBDIR=$(jemalloc-config --libdir)
+[ -n "$JEMALLOC_LIBDIR" ] || fail "Could not determine JEMALLOC_LIBDIR"
+JEMALLOC_INCDIR=$(jemalloc-config --includedir)
+[ -n "$JEMALLOC_INCDIR" ] || fail "Could not determine JEMALLOC_INCDIR"
+for a in "$JEMALLOC_LIBDIR"/libjemalloc*; do
+    bn=`basename $a`
+    info "Stripping $bn ..."
+    strip -g "$a" || fail "Failed to strip $a"
+done
+printok "jemalloc static library built and installed in $JEMALLOC_LIBDIR"
+
 info "Building RocksDB ..."
 cd "$top/$ROCKSDB_PACKAGE" || fail "Could not cd tp $ROCKSDB_PACKAGE"
-USE_RTTI=1 PORTABLE=1 make static_lib -j`nproc` V=1 || fail "Could not build RocksDB"
+USE_RTTI=1 PORTABLE=1 make static_lib -j`nproc` V=1 \
+    || fail "Could not build RocksDB"
 
 info "Stripping librocksdb.a ..."
 strip -g librocksdb.a || fail "Could not strip librocksdb.a"
@@ -31,7 +52,12 @@ printok "RocksDB built and moved to Fulcrum staticlibs directory"
 cd "$top"/"$PACKAGE" || fail "Could not chdir to Fulcrum dir"
 
 info "Building Fulcrum ..."
-qmake || fail "Could not run qmake"
+mkdir build && cd build || fail "Could not create/change-to build/"
+qmake ../Fulcrum.pro "CONFIG-=debug" \
+                     "CONFIG+=release" \
+                     "LIBS+=-L${JEMALLOC_LIBDIR} -ljemalloc" \
+                     "INCLUDEPATH+=${JEMALLOC_INCDIR}" \
+    || fail "Could not run qmake"
 make -j`nproc` || fail "Could not run make"
 
 ls -al "$TARGET_BINARY" || fail "$TARGET_BINARY not found"
