e89016fe31ecef94032f9816b1cc2250b86a5e1d
Mave95 <mave95@posteo.de>
2020-12-11 22:42:13 +0100

    fixed arithmetic expansion

--- contrib/base.sh
+++ contrib/base.sh
@@ -16,4 +16,4 @@ fi
 # If CPU_COUNT is still unset, default to 4
 export CPU_COUNT="${CPU_COUNT:-4}"
 # Use one more worker than core count
-export WORKER_COUNT=$[$CPU_COUNT+1]
+export WORKER_COUNT=$(($CPU_COUNT+1))
