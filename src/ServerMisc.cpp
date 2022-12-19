d3a486701acd620fc84844912b7adb99059b8f0a
Calin Culianu <calin.culianu@gmail.com>
2022-12-19 14:37:16 -0600

    Bumped protocol version to 1.5.0, minimum CashTokens version to 1.5.0

--- src/ServerMisc.cpp
+++ src/ServerMisc.cpp
@@ -4,8 +4,8 @@
 namespace ServerMisc
 {
     const Version MinProtocolVersion(1,4,0);
-    const Version MaxProtocolVersion(1,4,6);
-    const Version MinTokenAwareProtocolVersion(1,4,6);
+    const Version MaxProtocolVersion(1,5,0);
+    const Version MinTokenAwareProtocolVersion(1,5,0);
     const QString AppVersion(VERSION);
     const QString AppSubVersion = QString("%1 %2").arg(APPNAME, VERSION);
 }
