781785d1f67895b194404dd467da25945ade58d8
Calin Culianu <calin.culianu@gmail.com>
2023-11-09 07:29:14 +0200

    Add RPC method: blockchain.header.get
    This method allows clients to retrieve a particular header by its hash
    (or height, alternatively). The result is a dictionary containing keys
    "height" and "hex". "hex" is the header's 80-byte contents.
    
    Implements the suggestion in #191.

--- src/ServerMisc.cpp
+++ src/ServerMisc.cpp
@@ -4,7 +4,7 @@
 namespace ServerMisc
 {
     const Version MinProtocolVersion(1,4,0);
-    const Version MaxProtocolVersion(1,5,1);
+    const Version MaxProtocolVersion(1,5,2);
     const Version MinTokenAwareProtocolVersion(1,5,0);
     const QString AppVersion(VERSION);
     const QString AppSubVersion = QString("%1 %2").arg(APPNAME, VERSION);
