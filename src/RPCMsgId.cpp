35506a78f778f91b922a1f087f3d42abd77c92dc
Calin Culianu <calin.culianu@gmail.com>
2024-03-01 13:23:10 +0200

    Fixed a failing test: `rpcmsgid` for Linux

--- src/RPCMsgId.cpp
+++ src/RPCMsgId.cpp
@@ -170,9 +170,11 @@ namespace {
         CHK(RPCMsgId::fromVariant(2.0000000000000001) == RPCMsgId{2}); // impl. quirk: if the fractional part is too small, we map to integer :/
         CHK(RPCMsgId::fromVariant("2.0000000000000001") != RPCMsgId{2});
         CHK(RPCMsgId::fromVariant("2.0000000000000001").toString() ==  "2.0000000000000001");
-        CHK(Compat::GetVarType(r.toVariant()) == QMetaType::LongLong);
+        const auto metaTypeForInt64 = Compat::GetVarType(QVariant(int64_t{})); // this varies depending on platform, not always LongLong
+        CHK(metaTypeForInt64 == QMetaType::Long || metaTypeForInt64 == QMetaType::LongLong);
+        CHK(Compat::GetVarType(r.toVariant()) == metaTypeForInt64);
         CHK(Compat::GetVarType(RPCMsgId::fromVariant("123").toVariant()) == QMetaType::QString);
-        CHK(Compat::GetVarType(RPCMsgId::fromVariant(123.0).toVariant()) == QMetaType::LongLong);
+        CHK(Compat::GetVarType(RPCMsgId::fromVariant(123.0).toVariant()) == metaTypeForInt64);
         CHK(RPCMsgId::fromVariant(QVariant{}).toVariant().isNull());
         CHK(r.toVariant() == QVariant(123));
         CHK(r.toVariant() == QVariant(123.0));
